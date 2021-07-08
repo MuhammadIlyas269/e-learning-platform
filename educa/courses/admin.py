36f3d62285db81d9ad1e1f3a3071e8131bddfebd
Ilyas <MuhammadIlyas523221@gmail.comgit config --global user.name Ilyasexitgit config --global user.email MuhammadIlyas523221@gmail.com>
2021-07-09 02:15:39 +0500

    Add content model

--- educa/courses/admin.py
+++ educa/courses/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Subject, Course, Module
+from .models import(
+    Subject, Course, Module,
+    
+)
 
 @admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
